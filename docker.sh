4e5f163c838540291b7291b5235e76b01c3979a1
Ryan Sleevi <rsleevi@chromium.org>
2020-06-17 16:09:58 -0400

    Docker tweaks

--- docker.sh
+++ docker.sh
@@ -2,5 +2,3 @@
 
 tlmgr option repository http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2019/tlnet-final
 tlmgr --no-persistent-downloads install sourcecodepro sourcesanspro sourceserifpro crimsonpro
-cd /data
-make docs
