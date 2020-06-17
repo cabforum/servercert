5610e541a7c3fde1df9612d2a5e9d64ceb5bf67f
Ryan Sleevi <rsleevi@chromium.org>
2020-06-17 17:28:46 -0400

    Duplicate makefile

--- docker.sh
+++ docker.sh
@@ -2,4 +2,7 @@
 
 tlmgr option repository http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2019/tlnet-final
 tlmgr --no-persistent-downloads install sourcecodepro sourcesanspro sourceserifpro crimsonpro
-apt-get install --assume-yes --no-install-recommends make
+cd /data/docs
+pandoc -f markdown -t latex BR.md --table-of-contents -s --metadata-file ../assets/BR.yaml --pdf-engine=xelatex --template=../templates/BR.latex -o ../output/BR.pdf
+pandoc -f markdown -t html BR.md --table-of-contents -s --metadata-file ../assets/BR.yaml -o ../output/BR.html
+pandoc -f markdown -t docx BR.md --table-of-contents -s --metadata-file ../assets/BR.yaml --reference-doc=../templates/BR.docx -o ../output/BR.docx
