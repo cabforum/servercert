a63b6d915164dbb2e4ef892ff4e6ea122b3ef102
Ryan Sleevi <rsleevi@chromium.org>
2020-06-17 17:19:18 -0400

    Try to install make, maybe it will work

--- docker.sh
+++ docker.sh
@@ -2,3 +2,4 @@
 
 tlmgr option repository http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2019/tlnet-final
 tlmgr --no-persistent-downloads install sourcecodepro sourcesanspro sourceserifpro crimsonpro
+apt-get install --assume-yes --no-install-recommends make
