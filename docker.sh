0ddaeb5c5ff6d61666544c276ba78838187999e1
Jos Purvis <jopurvis@cisco.com>
2020-06-18 13:35:03 -0400

    Update docker.sh with make and cmds

--- docker.sh
+++ docker.sh
@@ -2,7 +2,6 @@
 
 tlmgr option repository http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2019/tlnet-final
 tlmgr --no-persistent-downloads install sourcecodepro sourcesanspro sourceserifpro crimsonpro
-cd /data/docs
-pandoc -f markdown -t latex BR.md --table-of-contents -s --metadata-file ../assets/BR.yaml --pdf-engine=xelatex --template=../templates/BR.latex -o ../output/BR.pdf
-pandoc -f markdown -t html BR.md --table-of-contents -s --metadata-file ../assets/BR.yaml -o ../output/BR.html
-pandoc -f markdown -t docx BR.md --table-of-contents -s --metadata-file ../assets/BR.yaml --reference-doc=../templates/BR.docx -o ../output/BR.docx
+apk add make
+cd /data
+make pandoc
