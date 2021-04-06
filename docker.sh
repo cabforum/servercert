9672b03bec91ad9a80f826e928e47f5c1f82964b
Ryan Sleevi <sleevi@google.com>
2021-04-06 14:19:53 -0400

    Remove legacy infrastructure tooling (#263)
    With the adoption of SC41, we can remove the Weasyprint and
    Kramdown infrastructure, as well as the related in-repository tools
    for building the docs. This has all been replaced with the
    https://github.com/cabforum/build-guidelines-action Docker image
    and GitHub Action.

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-tlmgr option repository http://ftp.math.utah.edu/pub/tex/historic/systems/texlive/2019/tlnet-final
-tlmgr --no-persistent-downloads install sourcecodepro sourcesanspro sourceserifpro crimsonpro
-apk add make
-cd /data
-make pandoc
